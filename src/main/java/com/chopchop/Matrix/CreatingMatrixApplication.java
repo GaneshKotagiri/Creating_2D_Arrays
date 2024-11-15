e53e39c530ac27ccb91ddfdad793821dc3d50d16
Ganesh Kotagiri <123595702+Ganeshkotagiri-12@users.noreply.github.com>
2024-11-15 13:37:05 -0600

    Java code for creating a 2D matrix

---
+++ src/main/java/com/chopchop/Matrix/CreatingMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.chopchop.Matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingMatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingMatrixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chopchop/Matrix/CreatingMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chopchop.Matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingMatrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
